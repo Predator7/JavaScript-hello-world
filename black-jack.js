ae3b8bb81c7569298fe5bd5c22eab314cea83dea
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-01-27 17:43:53 +0200

    Add card suites and values arrays

--- black-jack.js
+++ black-jack.js
@@ -1,3 +1,9 @@
 //
 // JavaScript Black Jack learning game
 //
+
+function createPlayingCards() {
+    let suites = ["Spades", "Hearts", "Clubs", "Diamonds"],
+        values = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+                  "Jack", "Queen", "King", "Ace"];
+}
