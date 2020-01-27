6456cc459a3b2cd34a494f702cf9ff73afb289c4
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-01-27 17:44:40 +0200

    Populate playingCards array with card objects

--- black-jack.js
+++ black-jack.js
@@ -6,4 +6,16 @@ function createPlayingCards() {
     let suites = ["Spades", "Hearts", "Clubs", "Diamonds"],
         values = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
                   "Jack", "Queen", "King", "Ace"];
+    
+    let playingCards = [];
+    for (let i = 0; i < values.length; i++) {
+        for (let j = 0; j < suites.length; j++) {
+            playingCards.push({
+                cardName: values[i] + " of " + suites[j]
+            });
+        }
+    }
+    return playingCards;
 }
+
+console.log(createPlayingCards());
